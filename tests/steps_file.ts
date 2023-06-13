2dee409cf4e74db80757f50d07c4530de6ea5bcd
Jeon Minji <mjjeon2645@gmail.com>
2023-06-13 17:31:27 +0900

    Codeceptjs 설치, 세팅파일 생성 및 설정

---
+++ tests/steps_file.ts
@@ -0,0 +1,20 @@
+// in this file you can append custom step methods to 'I' object
+
+export = () => actor({
+
+  // Define custom steps here, use 'this' to access default methods of I.
+  // It is recommended to place a general 'login' function here.
+
+});
+
+// Unable to compile TypeScript 에러 발생
+// module.exports = () => actor({
+
+// });
+
+// Unable to compile TypeScript 에러 발생
+// module.exports = function () {
+//   return actor({
+
+//   });
+// };
